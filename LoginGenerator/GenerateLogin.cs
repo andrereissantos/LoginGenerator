5aafa0ddd85d537239b1effc702f84d45dae4e1e
Andre Santos <andre.reis.santos@hotmail.com>
2016-10-05 15:46:21 -0300

    Este programa é um exemplo para gerar logins baseados no primeiro e último nome.

---
+++ LoginGenerator/GenerateLogin.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoginGenerator
+{
+    class Utils
+    {
+        public Utils()
+        {
+            this.LoadNames();
+        }
+        public List<string> AccountNames { get; set; }
+        private void LoadNames()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "nomes.txt";
+
+            if (!System.IO.File.Exists(path))
+                System.IO.File.CreateText(path);
+
+            string[] names = new System.IO.StreamReader(path).ReadToEnd().Split(',');
+
+            AccountNames = new List<string>();
+
+            foreach (var x in names)
+            {
+                AccountNames.Add(x.Replace("\"",""));
+            }
+        }
+        private bool CheckLogin(string login)
+        {
+            return AccountNames.Contains(login.ToLower());
+        }
+
+        public string GenerateLogin(string fullName)
+        {
+            //method signature to supress the byRef parameter
+            int i = 1;
+            return (GenerateLogin(fullName, ref i));
+        }
+        private string GenerateLogin(string fullName, ref int iteration)
+        {
+            
+            var login = "";
+            fullName = fullName.Trim();
+            string[] names = fullName.Split(' ');
+
+            if (fullName.Length == 0)
+                return Guid.NewGuid().ToString();
+
+            if (iteration > names[0].Length)
+            {
+                var iteration2 = iteration - names[0].Length;
+                if ((iteration - iteration2 - names[0].Length) == 0)
+                {
+                    login = names[0] + (iteration - names[0].Length).ToString();
+                }
+                else
+                {
+                    login = names[names.Length - 1] + names[0].Substring(0, iteration2);
+                }
+            }
+            else
+                login = names[0] + names[names.Length - 1].Substring(0, iteration);
+
+
+            if (CheckLogin(login.ToLower()))
+            {
+                iteration++;
+                return GenerateLogin(fullName, ref iteration);
+            }
+
+            return login;
+        }
+    }
+}
---
+++ LoginGenerator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+namespace LoginGenerator
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Utils utils = new Utils();
+            string helpText = "LoginGenerator \n"
+                            + "Este programa gera um nome de login a partir do primeiro e último nome.\n"
+                            + "Edite o arquivo NOMES.TXT para simular um nome existente \n\n\n"
+                            + "Comandos:\n\n\n"
+                            + "/? para exibir esta mensagem \n"
+                            + "+[nome] : Adiciona um nome à lista de nomes existentes (apenas na sessão)\n\n"
+                            + "/l: Exibe os nomes carregados na memória \n\n\n ";
+
+            Console.Clear();
+            Console.Write(helpText);
+
+            while (true)
+            {
+                Console.Write("Nome completo: ");
+                var str = Console.ReadLine();
+
+                if (str.Length == 0) { return; }
+
+                if (str.Substring(0, 1) == "+")
+                {
+                    utils.AccountNames.Add(str.Substring(1, str.Length - 1));
+                }
+
+                if (str == "/l")
+                {
+                    foreach (var name in utils.AccountNames)
+                        Console.WriteLine(name);
+                    continue;
+                }
+                if (str == "cls")
+                {
+                    Console.Clear();
+                    continue;
+                }
+                if (str == "/?")
+                {
+                    Console.WriteLine(helpText);
+                    continue;
+                }
+
+
+                Console.WriteLine(utils.GenerateLogin(str));
+
+
+            }
+        }
+    }
+}
